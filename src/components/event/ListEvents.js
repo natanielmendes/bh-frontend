0b4ff2b12d0df786e691e4a496ea66ee039d586f
Nataniel Carvalho <natanielmendes@hotmail.com>
2021-02-16 21:38:58 -0300

    Fetching events from mongodb

--- src/components/event/ListEvents.js
+++ src/components/event/ListEvents.js
@@ -3,7 +3,52 @@ import axios from 'axios';
 
 class ListEvents extends Component {
 
+    state = {
+        events: []
+    }
+
+    componentDidMount = () => {
+        axios.get('http://localhost:3001/event').then((res) => {
+            const fetchedEvents = [];
+                for (let key in res.data) {
+                    fetchedEvents.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+            this.setState({events: fetchedEvents});
+            console.log(this.state.events)
+        });
+    }
+
+    componentDidUpdate(prevState, nextState) {
+        if (!Object.keys(prevState).length === 0 && prevState !== nextState) {
+            axios.get('http://localhost:3001/event').then((res) => {
+                const fetchedEvents = [];
+                    for (let key in res.data) {
+                        fetchedEvents.push({
+                            ...res.data[key],
+                            id: key
+                        });
+                    }
+                this.setState({events: fetchedEvents});
+                console.log(this.state.events)
+            });
+        }
+    }
+
     render() {
+        let events = []
+        for (let eventData of this.state.events) {
+            events.push(
+                <tr>
+                    <th scope="row">{eventData.firstName}</th>
+                    <td>{eventData.lastName}</td>
+                    <td>{eventData.email}</td>
+                    <td>{eventData.eventDate}</td>
+                </tr>
+            )
+        }
         return (
             <table class="table table-hover">
                 <thead>
@@ -15,24 +60,7 @@ class ListEvents extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry the Bird</td>
-                        <td>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    {events}
                 </tbody>
             </table>
         )
